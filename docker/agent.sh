efdcc0629624cda6935b41fda7c2c82413eef684
tubame <tubame@example.com>
2022-03-29 05:36:04 +0000

    fix scripts.

--- docker/agent.sh
+++ docker/agent.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -n "$AWS_S3_ENDPOINT" ] && [ -n "$AWS_APIGW_ENDPOINT" ] && [ -n "$AWS_REGION" ]; then
-  sed -e "s/\${AWS_APIGW_ENDPOINT}/${AWS_APIGW_ENDPOINT}/g" /etc/envoy/envoy.tmpl | sed -e "s/\${AWS_S3_ENDPOINT}/${AWS_S3_ENDPOINT}/g" | sed -e "s/\${AWS_REGION}/${AWS_REGION}/g" > /etc/envoy/envoy.yaml
+  eval "cat <<< \"$(cat /etc/envoy/envoy.tmpl)\"" > /etc/envoy/envoy.yaml
 else
-  echo "AWS_S3_ENDPOINT, AWS_APIGW_ENDPOINT are not defined."
+  echo "AWS_S3_ENDPOINT, AWS_APIGW_ENDPOINT AWS_REGION are not defined."
   exit -1
 fi
 
