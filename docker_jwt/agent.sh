03f99393bdc96aeb5d820c70d74815ab1f55c9fd
tubame <25952690+tubame0505@users.noreply.github.com>
2022-04-08 06:53:51 +0000

    Update 'docker_jwt' - add token_use check.

--- docker_jwt/agent.sh
+++ docker_jwt/agent.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if [ -n "$AWS_S3_ENDPOINT" ] && [ -n "$AWS_APIGW_ENDPOINT" ] && [ -n "$AWS_REGION" ] && [ -n "$JWT_ISSUER" ] && [ -n "$JWT_JWK_URI" ] && [ -n "$JWT_ISSUER_HOST" ]; then
+if [ -n "$AWS_S3_ENDPOINT" ] && [ -n "$AWS_APIGW_ENDPOINT" ] && [ -n "$AWS_REGION" ] && [ -n "$JWT_ISSUER" ] && [ -n "$JWT_CLIENT" ]&& [ -n "$JWT_JWK_URI" ] && [ -n "$JWT_ISSUER_HOST" ]; then
   eval "cat <<< \"$(cat /etc/envoy/envoy.tmpl)\"" > /etc/envoy/envoy.yaml
 else
-  echo "AWS_S3_ENDPOINT, AWS_APIGW_ENDPOINT AWS_REGION JWT_ISSUER JWT_JWK_URI JWT_ISSUER_HOST are not defined."
+  echo "AWS_S3_ENDPOINT, AWS_APIGW_ENDPOINT AWS_REGION JWT_ISSUER JWT_JWK_URI JWT_ISSUER_HOST JWT_CLIENT are not defined."
   exit -1
 fi
 
